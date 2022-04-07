6629abcec74988fda4e7656f7ac8c06ba83a4c99
Oke Edafe Great <greatedafeoke@gmail.com>
2022-04-07 11:47:40 -0700

    Added 0-pascal_triangle.py and 0-main.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+A module that contains the pascal_triangle function
+'''
+
+from math import factorial
+
+
+def combination(a, b):
+    return int(factorial(a) / (factorial(b) * factorial(a - b)))
+
+def pascal_triangle(n):
+    '''
+    A function that returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    '''
+
+    pascal_list = []
+
+    if n <= 0:
+        return pascal_list
+
+    for i in range(n):
+        y = 0
+        row = []
+        for j in range(i + 1):
+            row.append(combination(i, y))
+            y += 1
+        pascal_list.append(row)
+    return pascal_list
